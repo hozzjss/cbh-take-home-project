9a5630d3b95ffae8cb0b8fd6b900d496729631d8
hz <13457698+hozzjss@users.noreply.github.com>
2023-02-11 23:41:05 +0200

    first refactor run

--- dpk.js
+++ dpk.js
@@ -1,28 +1,22 @@
 const crypto = require("crypto");
 
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+const sha3 = (data) => crypto.createHash("sha3-512").update(data).digest("hex");
+
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
+  let candidate = TRIVIAL_PARTITION_KEY;
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+    candidate = event.partitionKey;
+    candidate = candidate || sha3(JSON.stringify(event));
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
   }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return sha3(data);
   }
   return candidate;
 };
